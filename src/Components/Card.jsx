a08e02ae975cc26348869b07d1a877a4faa842b6
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2025-10-04 01:30:26 +0600

    fifth commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { Calendar } from "lucide-react";
+import { ToastContainer } from 'react-toastify';
+import TaskStatus from "./TaskStatus"
+import Container from './Container';
+const Card = ({ ticketPromise, setResolved, setProgress }) => {
+    const [tickets, setTickets] = useState([]);
+    const [taskStatus, setTaskStatus] = useState([]);
+    const [resolvedTasks, setResolvedTasks] = useState([]);
 
-const Card = ({ ticketPromise, setResolved }) => {
+    const statusColors = {
+        Open: "bg-green-100 text-green-700",
+        "In-Progress": "bg-yellow-100 text-yellow-600",
+    };
+
+    useEffect(() => {
+        ticketPromise.then(data => setTickets(data || []));
+    }, [ticketPromise]);
+
+    useEffect(() => {
+        if (setProgress ) {
+            setProgress (taskStatus.length);
+        } else if (setResolved) {
+            setResolved(resolvedTasks.length);
+        }
+    }, [taskStatus, resolvedTasks, setProgress , setResolved]);
+    const handleTask = (ticket) => {
+        const findtaskStatus = taskStatus.find(el => el.id === ticket.id);
+        const filtertickets = tickets.filter(el => el.id !== ticket.id)
+        if (findtaskStatus) {
+            toast.warning("This ticket already in Task Status!");
+            return;
+        }
+        setTaskStatus([...taskStatus, ticket]);
+        setTickets(filtertickets);
+        toast.success("Added to Task Status");
+        toast.info("The In Progress count increases");
+        toast.info("It is removed from the Customer Tickets list")
+    }
+    if (!tickets || tickets.length === 0) {
+        return <div className='text-center py-10 text-gray-400'>No tickets found</div>
+    }
     return (
-        <div>
+        <>
+            <h2 className='text-xl font-bold px-7'>Customer Tickets</h2>
+            <div className='grid grid-col-1 lg:grid-cols-3 gap-8 p-6'>
+                <div className='lg:col-span-2'>
+                    <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-4 mb-5'>
+                        {tickets.map(el => (
+                            <div key={el.id} className='bg-white shadow rounded-lg p-4 border border-gray-200 w-full hover:shadaw-md cursor-pointer' onClick={() => handleTask(el)}>
+                                <Container>
+                                    <div className='flex justify-center items-start'>
+                                        <h3 className='text-lg font-semibold text-gray-800'>{el.title}</h3>
+                                        <span className={`flex items-center gap-1 px-3 py-1 text-[14px] font-medium rounded-full ${statusColors[el.status]}`}>
+                                            <span className='h-2 w-2 rounded-full'></span>
+                                            {el.status}
+                                        </span>
+                                    </div>
+                                    <p className='text-gray-600 text-[16px] mt-2'>{el.description}</p>
+                                    <div className='flex justify-center items-start mt-4 text-sm'>
+                                        <div className='flex flex-row gap-1 mt-2 items-start'>
+                                            <span className="text-gray-600 font-medium">{el.id}</span>
+                                            <span className={`${el.priority === "High" ? "text-red-600" : "text-yellow-500"} font-semibold`}>
+                                                {el.priority.toUpperCase()} PRIORITY
+                                            </span>
+                                        </div>
 
-        </div>
+
+                                        <div className='flex flex-row mt-2 gap-2 itmes-end'>
+                                            <span className="text-gray-700">{el.customer}</span>
+                                            <div className="flex items-center text-gray-500 gap-1">
+                                                <Calendar size={14} />
+                                                <span>{el.createdAt}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Container>
+                            </div>
+                        ))
+
+                        }
+                    </div>
+                </div>
+
+                <TaskStatus taskStatus={taskStatus} setTaskStatus={setTaskStatus} resolvedTasks={resolvedTasks} setResolvedTasks={setResolvedTasks} />
+                <ToastContainer position="top-right" autoClose={1000} />
+            </div>
+        </>
     );
 };
 
 export default Card;
+
+
+
---
+++ src/Components/TaskStatus.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+import Container from './Container';
+
+const TaskStatus = ({ taskStatus, setTaskStatus, resolvedTasks, setResolvedTask }) => {
+    const handleCompleteTask = cTask => {
+        const res = taskStatus.filter(el => el.id !== cTask.id)
+        setTaskStatus(res);
+        setResolvedTask([...resolvedTasks, cTask]);
+        toast.info("Completed");
+        toast.info("In Progess count decreased");
+        toast.success("Resolved count increased")
+    }
+    return (
+        <div className=" pt-0  gap-8 p-4 px-5 mb-0 lg:w-[480px] md:w-[480px]">
+            <h2 className="text-lg font-semibold text-gray-800 mb-5 pt-0">Task Status</h2>
+            <Container>
+                {taskStatus.length > 0 ? (
+                    taskStatus.map((task) => (
+                        <div key={task.id} className="bg-white p-3 mb-[12px] border border-white shadow-sm">
+                            <p className="text-[18px] font-semibold md:px-5 text-gray-800 ">{task.title}</p>
+                            <div className="px-0 sm:w-22 sm:px-5">
+                                <button
+                                    onClick={() => handleCompleteTask(task)}
+                                    className="bg-green-500 hover:bg-green-600 text-white text-[16px] p-4 py-1 mt-2 rounded sm:px-5  lg:w-[380px] h-[35px]"
+                                >
+                                    Complete
+                                </button>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-sm text-gray-600 mb-4">
+                        Select a ticket to add to Task Status
+                    </p>
+                )}
+
+                <h3 className="text-md font-semibold text-gray-700 mt-4">Resolved Task</h3>
+                {resolvedTasks.length > 0 ? (
+                    <ul className="mt-2 space-y-2">
+                        {resolvedTasks.map((task) => (
+                            <div key={task.id} className="bg-green-50 p-3 mb-3 rounded h-[] shadow-sm">
+                                <p className="text-[16px] font-medium text-gray-800">{task.title}</p>
+                            </div>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="text-sm text-gray-600">No resolved tasks yet.</p>
+                )}
+            </Container>
+        </div>
+    );
+};
+
+export default TaskStatus;
